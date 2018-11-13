b4346537a873343ad1714f94ff10281803eb1eb3
Mahia Tasneem <mahiatasneem@mahiatasneem-macbookpro2.roam.corp.google.com>
2018-11-13 15:37:54 -0800

    Added homepage UI for the website with login authentication

---
+++ public/main.js
@@ -0,0 +1,33 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+function loginFunction(){
+	console.log(provider);
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  console.log(user);
+	  console.log("is logged in");
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+}
+$(document).ready(function(){
+ $('.header').height($(window).height());
+})
+
+/*Scroll effect*/
+$(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+  
+ })
