37e62aa8c27a4183b39422a3ba437e60b8e15793
Mahia Tasneem <mahia.tasneem@bison.howard.edu>
2018-11-30 11:43:15 -0800

    Added subpages.

--- public/main.js
+++ public/main.js
@@ -30,4 +30,49 @@ $(".navbar a").click(function(){
    scrollTop:$("#" + $(this).data('value')).offset().top
   },1000)
   
- })
+})
+ 
+function ReadFromDatabase(){
+ 	var userID = firebase.auth().currentUser.uid;
+ 	var database = firebase.database();
+ 	firebase.database().ref('users').once('value').then(function(snapshot){
+ 		console.log(snapshot.val());
+ 		});
+ }
+ 
+function GetChallenge(){
+ 	//var userID = firebase.auth().currentUser.uid;
+ 	var database = firebase.database();
+ 	firebase.database().ref('Arrow Quiz 1').on('value',function(snapshot){
+ 		console.log(snapshot.val()["Questions"]);
+ 		var QuestionDictionary = snapshot.val()["Questions"];
+ 		var table = document.getElementById("QuestionsTable");
+ 		//var row = table.insertRow(0);
+ 		//var cell = row.insertCell(0);
+ 		//cell.innerHTML = "ARROW QUIZ!"
+ 		var i = 0;
+ 		for(var key in QuestionDictionary){
+ 			if(QuestionDictionary.hasOwnProperty(key)){
+ 				var row = table.insertRow(i);
+ 				var cell = row.insertCell();
+ 				cell.innerHTML = QuestionDictionary[key];
+ 			}
+ 			i++;
+ 		}
+ 		});
+ }
+ 
+ function NewQuestion(){
+ 	var newQuestionKey = firebase.database().ref().child('Arrow Quiz 1/Questions').push().key;
+ 	firebase.database().ref('Arrow Quiz 1/Questions/' + newQuestionKey).set("Olicity");
+ }
+ 
+ var field = 1;
+ function add_fields() {
+     field++;
+     var objTo = document.getElementById('room_fileds')
+     var divtest = document.createElement("div");
+     divtest.innerHTML = '<div class="label">Room ' + field +':</div><div class="content"><span>Width: <input type="text" style="width:48px;" name="width[]" value="" /><small>(ft)</small> X</span><span>Length: <input type="text" style="width:48px;" namae="length[]" value="" /><small>(ft)</small></span></div>';
+
+     objTo.appendChild(divtest)
+ }
