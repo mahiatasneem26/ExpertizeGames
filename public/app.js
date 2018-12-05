23cfde6be7f16f06d1478acaca03940abc9a13fc
Mahia Tasneem <mahia.tasneem@bison.howard.edu>
2018-12-04 16:56:01 -0800

    Enabled facebook and twitter login authentication:

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
-var provider = new firebase.auth.GoogleAuthProvider();
+var Gprovider = new firebase.auth.GoogleAuthProvider();
 
 function loginFunction(){
-	console.log(provider);
-	firebase.auth().signInWithPopup(provider).then(function(result) {
+	console.log(Gprovider);
+	firebase.auth().signInWithPopup(Gprovider).then(function(result) {
 	  // This gives you a Google Access Token. You can use it to access the Google API.
 	  var token = result.credential.accessToken;
 	  // The signed-in user info.
@@ -23,6 +23,63 @@ function loginFunction(){
 	ReadFromDatabase();
 
 	console.log("sup dude!")
+}
+
+var fbProvider = new firebase.auth.FacebookAuthProvider();
+
+function fbLogin(){
+	firebase.auth().signInWithPopup(fbProvider).then(function(result) {
+	  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+}
+
+var response = {
+    status: 'connected',
+    authResponse: {
+        accessToken: '...',
+        expiresIn:'...',
+        signedRequest:'...',
+        userID:'...'
+    }
+}
+
+// FB.getLoginStatus(function(response) {
+//     statusChangeCallback(response);
+// });
+
+
+var twitterProvider = new firebase.auth.TwitterAuthProvider();
+
+function twitterLogin(){
+	firebase.auth().signInWithPopup(twitterProvider).then(function(result) {
+	  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
 }
 
 function ReadFromDatabase(){
